c4007cbd2ec6794ab1e1d59a2d05f3864535a11b
lctech-roy <Roy@lctech.com.tw>
2022-04-02 17:17:59 +0800

    Add transaction

--- Migration/Program.cs
+++ Migration/Program.cs
@@ -28,7 +28,7 @@ var mapperList = new List<IMapper>()
      //界面 » 在線列表圖標
      new OnlineMemberIconMapper("onlineMemberIcon"),
      //會員 » 禁止 IP
-     new BannedIpMapper("bannedIp"),
+     //new BannedIpMapper("bannedIp"),
      //會員 » 推薦關注
      new RecommendFollowMapper("recommendMember", "follow"),
      //會員 » 推薦好友
@@ -36,7 +36,7 @@ var mapperList = new List<IMapper>()
      //運營 » 站點幫助
      new ForumFaqMapper("forumFaq"),
      //運營 » 友情連接
-    new FriendSiteLinkMapper("friendSiteLink")
+     //new FriendSiteLinkMapper("friendSiteLink")
 };
 
 var configurationList = new List<Configuration>();
@@ -47,10 +47,12 @@ mapperList.ForEach(mapper =>
 });
 
 using var pgSqlConnection = new NpgsqlConnection(config.GetSection(pgSqlConnectionStr).Value);
+pgSqlConnection.Open();
+using var trans = pgSqlConnection.BeginTransaction();
 
 foreach (var configuration in configurationList)
 {
-    Console.WriteLine("key" + configuration.Key);
+    Console.WriteLine(configuration.Key);
     pgSqlConnection.Execute(
         @"INSERT INTO ""Configuration"" (""Id"",""Key"", ""Value"", ""Group"",""Source"", ""ParentId""
                                         , ""CreationDate"", ""CreatorId"", ""ModificationDate"", ""ModifierId"",
@@ -58,3 +60,6 @@ foreach (var configuration in configurationList)
         VALUES (@Id,@Key,@Value,@Group,@Source,@ParentId,@CreationDate,@CreatorId,@ModificationDate,
                 @ModifierId,@Hierarchy,@Level,@SortingIndex,@Version);", configuration);
 }
+
+trans.Commit();
+Console.WriteLine("Migration Finish!");
